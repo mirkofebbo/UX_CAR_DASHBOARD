d58554dddee5fb368c0d2b6e330ff22db27b9f59
Mirko Febbo <56055818+mirkofebbo@users.noreply.github.com>
2023-06-02 12:54:10 +0100

    local host

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-const SERVER_IP = '158.223.47.108'; // replace with your server's IP address
+const SERVER_IP = 'localhost'; // replace with your server's IP address
 //if you are just hosting this on your machine use: localhost
 module.exports = {
     SERVER_IP
